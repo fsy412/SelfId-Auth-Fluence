217704bc1ea9d4ee249442eafc74e307b78ef171
fsy412 <fsy412@gmail.com>
2021-11-25 12:28:38 +0800

    Update deploy.sh

--- services/auth/scripts/deploy.sh
+++ services/auth/scripts/deploy.sh
@@ -1,4 +1,4 @@
 fldist --node-id 12D3KooWFEwNWcHqi9rtsmDhsYcDbRUCDXH84RC4FW6UfsFWaoHi \
        new_service \
-       --ms artifacts/auth_service.wasm:configs/add_service_cfg.json \
+       --ms artifacts/auth_service.wasm:configs/auth_service_cfg.json \
        --name auth
